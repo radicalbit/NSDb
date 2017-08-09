f2b7b626533943e14934bcca14047ff152c9b1bb
Saverio Veltri <saverio.veltri@radicalbit.io>
2017-08-09 11:40:57 +0200

    Second version of statement parser

--- nsdb-core/src/main/scala/io/radicalbit/nsdb/index/IndexerActor.scala
+++ nsdb-core/src/main/scala/io/radicalbit/nsdb/index/IndexerActor.scala
@@ -6,17 +6,15 @@ import akka.actor.{Actor, Props}
 import io.radicalbit.index.BoundedIndex
 import io.radicalbit.nsdb.coordinator.ReadCoordinator
 import io.radicalbit.nsdb.model.Record
-import io.radicalbit.nsdb.statement._
-import org.apache.lucene.document.LongPoint
-import org.apache.lucene.search.{BooleanClause, BooleanQuery, Query}
+import io.radicalbit.nsdb.statement.StatementParser
 import org.apache.lucene.store.FSDirectory
-
-import scala.util.{Failure, Success, Try}
 
 class IndexerActor(basePath: String) extends Actor {
   import io.radicalbit.nsdb.index.IndexerActor._
 
   import scala.collection.mutable
+
+  private val statementParser = new StatementParser()
 
   private val indexes: mutable.Map[String, BoundedIndex] = mutable.Map.empty
 
@@ -27,44 +25,6 @@ class IndexerActor(basePath: String) extends Actor {
       indexes + (metric -> newIndex)
       newIndex
     })
-
-  private def parseExpression(exp: Expression): Query = {
-    exp match {
-      case ComparisonExpression(dimension, operator: ComparisonOperator, value: Long) => {
-        operator match {
-          case GreaterThanOperator      => LongPoint.newRangeQuery(dimension, value + 1, Long.MaxValue)
-          case GreaterOrEqualToOperator => LongPoint.newRangeQuery(dimension, value, Long.MaxValue)
-          case LessThanOperator         => LongPoint.newRangeQuery(dimension, 0, value - 1)
-          case LessOrEqualToOperator    => LongPoint.newRangeQuery(dimension, 0, value)
-        }
-      }
-      case RangeExpression(dimension, v1: Long, v2: Long) => LongPoint.newRangeQuery(dimension, v1, v2)
-      case UnaryLogicalExpression(expression, _) =>
-        val builder = new BooleanQuery.Builder()
-        builder.add(parseExpression(expression), BooleanClause.Occur.MUST_NOT).build()
-      case TupledLogicalExpression(expression1, operator: TupledLogicalOperator, expression2: Expression) =>
-        operator match {
-          case AndOperator =>
-            val builder = new BooleanQuery.Builder()
-            builder.add(parseExpression(expression1), BooleanClause.Occur.MUST)
-            builder.add(parseExpression(expression2), BooleanClause.Occur.MUST).build()
-          case OrOperator =>
-            val builder = new BooleanQuery.Builder()
-            builder.add(parseExpression(expression1), BooleanClause.Occur.SHOULD)
-            builder.add(parseExpression(expression2), BooleanClause.Occur.SHOULD).build()
-        }
-    }
-  }
-
-  private def parseStatement(statament: SelectSQLStatement): Try[Query] = {
-
-    (statament.limit, statament.condition) match {
-      case (Some(limit), Some(condition)) =>
-        Success(parseExpression(condition.expression))
-      case _ => Failure(new RuntimeException("cannot execute query withour a limit or a condition"))
-    }
-
-  }
 
   override def receive: Receive = {
     case AddRecord(metric, record) =>
@@ -92,8 +52,8 @@ class IndexerActor(basePath: String) extends Actor {
       val hits  = index.timeRange(0, Long.MaxValue)
       sender ! CountGot(metric, hits.size)
     case ReadCoordinator.ExecuteSelectStatement(statement) => {
-      val query = parseStatement(statement).get
-      getIndex(statement.metric).query(query, statement.limit.get.value, None)
+      val queryResult = statementParser.parseStatement(statement).get
+      getIndex(statement.metric).query(queryResult.q, queryResult.limit, queryResult.sort)
     }
   }
 }
---
+++ nsdb-core/src/main/scala/io/radicalbit/nsdb/statement/StatementParser.scala
@@ -0,0 +1,55 @@
+package io.radicalbit.nsdb.statement
+
+import io.radicalbit.nsdb.statement.StatementParser.QueryResult
+import org.apache.lucene.document.LongPoint
+import org.apache.lucene.search._
+
+import scala.util.{Failure, Success, Try}
+
+class StatementParser {
+
+  private def parseExpression(exp: Option[Expression], limit: Int): QueryResult = {
+    val q = exp match {
+      case Some(ComparisonExpression(dimension, operator: ComparisonOperator, value: Long)) => {
+        operator match {
+          case GreaterThanOperator      => LongPoint.newRangeQuery(dimension, value + 1, Long.MaxValue)
+          case GreaterOrEqualToOperator => LongPoint.newRangeQuery(dimension, value, Long.MaxValue)
+          case LessThanOperator         => LongPoint.newRangeQuery(dimension, 0, value - 1)
+          case LessOrEqualToOperator    => LongPoint.newRangeQuery(dimension, 0, value)
+        }
+      }
+      case Some(RangeExpression(dimension, v1: Long, v2: Long)) => LongPoint.newRangeQuery(dimension, v1, v2)
+      case Some(UnaryLogicalExpression(expression, _)) =>
+        val builder = new BooleanQuery.Builder()
+        builder.add(parseExpression(Some(expression), limit).q, BooleanClause.Occur.MUST_NOT).build()
+      case Some(TupledLogicalExpression(expression1, operator: TupledLogicalOperator, expression2: Expression)) =>
+        operator match {
+          case AndOperator =>
+            val builder = new BooleanQuery.Builder()
+            builder.add(parseExpression(Some(expression1), limit).q, BooleanClause.Occur.MUST)
+            builder.add(parseExpression(Some(expression2), limit).q, BooleanClause.Occur.MUST).build()
+          case OrOperator =>
+            val builder = new BooleanQuery.Builder()
+            builder.add(parseExpression(Some(expression1), limit).q, BooleanClause.Occur.SHOULD)
+            builder.add(parseExpression(Some(expression2), limit).q, BooleanClause.Occur.SHOULD).build()
+        }
+      case None => new MatchAllDocsQuery()
+    }
+    QueryResult(q, limit)
+  }
+
+  def parseStatement(statement: SelectSQLStatement): Try[QueryResult] = {
+
+    statement.limit match {
+      case (Some(limit)) =>
+        Success(parseExpression(statement.condition.map(_.expression), limit.value))
+      case _ => Failure(new RuntimeException("cannot execute query without a limit"))
+    }
+
+  }
+
+}
+
+object StatementParser {
+  case class QueryResult(q: Query, limit: Int, sort: Option[Sort] = None)
+}
