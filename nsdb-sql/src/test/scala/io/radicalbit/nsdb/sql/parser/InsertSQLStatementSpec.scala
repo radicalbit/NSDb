9224e73506db5a9281dee9c60682323c8f2c4ec3
Saverio Veltri <saverio.veltri@radicalbit.io>
2017-08-30 11:53:35 +0200

    add float in parser

--- nsdb-sql/src/test/scala/io/radicalbit/nsdb/sql/parser/InsertSQLStatementSpec.scala
+++ nsdb-sql/src/test/scala/io/radicalbit/nsdb/sql/parser/InsertSQLStatementSpec.scala
@@ -85,6 +85,21 @@ class InsertSQLStatementSpec extends WordSpec with Matchers {
       }
     }
 
+    "receive an insert with multiple dimensions in int and float format and a timestamp" should {
+      "parse it successfully" in {
+        parser.parse(namespace = "registry", input = "INSERT INTO people TS=123456 DIM(x=1, y=1.5) FLD(value=23) ") should be(
+          Success(
+            InsertSQLStatement(
+              namespace = "registry",
+              metric = "people",
+              timestamp = Some(123456),
+              dimensions = ListAssignment(Map("x" -> 1, "y" -> 1.5)),
+              fields = ListAssignment(Map("value" -> 23))
+            ))
+        )
+      }
+    }
+
     "receive a wrong metric without dimensions" should {
       "fail" in {
         parser.parse(namespace = "registry", input = "INSERT INTO people FLD(value=23) ") shouldBe 'failure
