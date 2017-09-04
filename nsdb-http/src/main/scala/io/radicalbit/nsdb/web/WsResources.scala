d526030361d873fe4de31034f8f7a1691da04cbb
Saverio Veltri <saverio.veltri@radicalbit.io>
2017-09-04 14:34:16 +0200

    fix streaming messages parsing

--- nsdb-http/src/main/scala/io/radicalbit/nsdb/web/WsResources.scala
+++ nsdb-http/src/main/scala/io/radicalbit/nsdb/web/WsResources.scala
@@ -10,7 +10,7 @@ import akka.stream.scaladsl.{Flow, Sink, Source}
 import io.radicalbit.nsdb.web.actor.StreamActor
 import io.radicalbit.nsdb.web.actor.StreamActor._
 import org.json4s._
-import org.json4s.native.JsonMethods._
+import org.json4s.native.JsonMethods.{parse, _}
 import org.json4s.native.Serialization.write
 
 trait WsResources {
@@ -27,9 +27,10 @@ trait WsResources {
       Flow[Message]
         .map {
           case TextMessage.Strict(text) =>
-            parse(text).extractOpt[RegisterQuery] orElse parse(text).extractOpt[RegisterQueries] orElse
-              parse(text).extractOpt[RegisterQuid] orElse parse(text)
-              .extractOpt[RegisterQuids] getOrElse "Message not handled by receiver"
+            parse(text).extractOpt[RegisterQuery] orElse
+              parse(text).extractOpt[RegisterQueries] orElse
+              parse(text).extractOpt[RegisterQuid] orElse
+              parse(text).extractOpt[RegisterQuids] getOrElse "Message not handled by receiver"
         }
         .to(Sink.actorRef(connectedWsActor, Terminate))
 
