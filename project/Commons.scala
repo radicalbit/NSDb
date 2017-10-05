4adb44043f887a436242434ee37556e50dbb9391
Mauro Cortellazzi <mauro.cortellazzi@radicalbit.io>
2017-10-05 15:12:18 +0200

    updated internal repo refs

--- project/Commons.scala
+++ project/Commons.scala
@@ -13,7 +13,7 @@ object Commons {
     organization := "io.radicalbit.nsdb",
     resolvers ++= Seq(
       Opts.resolver.mavenLocalFile,
-      "Radicalbit Repo" at "https://tools.radicalbit.io/maven/repository/internal/",
+      "Radicalbit Repo" at "https://tools.radicalbit.io/artifactory/libs-release-local/",
       Resolver.bintrayRepo("hseeberger", "maven"),
       "krasserm at bintray" at "http://dl.bintray.com/krasserm/maven"
     ),
