b579da5fce14ad39298efc6569f7c5fa1fb14aff
Saverio Veltri <saverio.veltri@radicalbit.io>
2017-10-26 23:39:03 +0200

    integrate metadata coordinator with cache

--- nsdb-cluster/src/main/scala/io/radicalbit/nsdb/cluster/actor/MetadataCoordinator.scala
+++ nsdb-cluster/src/main/scala/io/radicalbit/nsdb/cluster/actor/MetadataCoordinator.scala
@@ -1,25 +1,48 @@
 package io.radicalbit.nsdb.cluster.actor
 
+import java.util.concurrent.TimeUnit
+
 import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 import akka.cluster.pubsub.DistributedPubSub
-import akka.cluster.pubsub.DistributedPubSubMediator.{Publish, Send}
-import io.radicalbit.nsdb.cluster.actor.MetadataCoordinator.commands.{AddLocation, AddLocations, UpdateLocation}
+import akka.cluster.pubsub.DistributedPubSubMediator.Publish
+import akka.pattern.{ask, pipe}
+import akka.util.Timeout
+import io.radicalbit.nsdb.cluster.actor.MetadataCoordinator.commands.{AddLocation, UpdateLocation}
+import io.radicalbit.nsdb.cluster.actor.MetadataCoordinator.events.{AddLocationFailed, LocationAdded}
+import io.radicalbit.nsdb.cluster.actor.ReplicatedMetadataCache.{Cached, PutInCache}
 import io.radicalbit.nsdb.cluster.index.Location
-
-import scala.collection.mutable
 
 class MetadataCoordinator(cache: ActorRef) extends Actor with ActorLogging {
 
-  private val metadatas: mutable.Map[String, Seq[Location]] = mutable.Map.empty
+  val mediator: ActorRef = DistributedPubSub(context.system).mediator
 
-  val mediator = DistributedPubSub(context.system).mediator
+  implicit val timeout: Timeout = Timeout(
+    context.system.settings.config.getDuration("nsdb.write-coordinator.timeout", TimeUnit.SECONDS),
+    TimeUnit.SECONDS)
+  import context.dispatcher
 
   override def receive: Receive = {
     case msg @ AddLocation(namespace, location, occurredOn) =>
-      mediator ! Publish("metadata", msg)
-    case msg @ AddLocations(namespace, locations, occurredOn) =>
-      mediator ! Publish("metadata", msg)
-    case msg @ UpdateLocation(_, _, _, _) =>
+      (mediator ? PutInCache(Key(namespace,location.metric,location.from, location.to), location))
+        .map{
+        case Cached(_, Some(_)) =>
+          mediator ! Publish("metadata", msg)
+          LocationAdded(namespace,location,occurredOn)
+        case _ => AddLocationFailed(namespace,location,occurredOn)
+      }
+        .pipeTo(sender)
+
+
+    case msg @ UpdateLocation(namespace, oldLocation, newOccupation, occurredOn) =>
+      val newLocation = oldLocation.copy(occupied = newOccupation)
+      (mediator ? PutInCache(Key(namespace,oldLocation.metric,oldLocation.from, oldLocation.to), newLocation))
+        .map{
+          case Cached(_, Some(_)) =>
+            mediator ! Publish("metadata", msg)
+            LocationAdded(namespace,newLocation,occurredOn)
+          case _ => AddLocationFailed(namespace,newLocation,occurredOn)
+        }
+        .pipeTo(sender)
       mediator ! Publish("metadata", msg)
   }
 }
@@ -53,7 +76,9 @@ object MetadataCoordinator {
                            location: Option[Location],
                            occurredOn: Long)
     case class LocationUpdated(namespace: String, oldLocation: Location, newOccupation: Long, occurredOn: Long)
+    case class UpdateLocationFailed(namespace: String, oldLocation: Location, newOccupation: Long, occurredOn: Long)
     case class LocationAdded(namespace: String, location: Location, occurredOn: Long)
+    case class AddLocationFailed(namespace: String, location: Location, occurredOn: Long)
     case class LocationsAdded(namespace: String, locations: Seq[Location], occurredOn: Long)
     case class LocationDeleted(namespace: String, location: Location, occurredOn: Long)
     case class NamespaceDeleted(namespace: String, occurredOn: Long)
