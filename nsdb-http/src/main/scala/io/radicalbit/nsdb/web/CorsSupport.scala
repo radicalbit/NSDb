967e5ca7b0f7d7d4e9d624ae5143f6cc326d6787
Saverio Veltri <saverio.veltri@radicalbit.io>
2017-11-14 15:37:33 +0100

    hotfix

--- nsdb-http/src/main/scala/io/radicalbit/nsdb/web/CorsSupport.scala
+++ nsdb-http/src/main/scala/io/radicalbit/nsdb/web/CorsSupport.scala
@@ -12,7 +12,7 @@ trait CorsSupport {
 
   val corsHeaders = List(RawHeader("Access-Control-Allow-Origin", "*"),
     RawHeader("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS, DELETE"),
-    RawHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization") )
+    RawHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Origin, Origin, X-Requested-With, Content-Type, Accept, Authorization") )
 
   def withCors(route: Route) = respondWithHeaders(corsHeaders) {route ~ optionsSupport}
 
