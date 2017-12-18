16be9f223723cc6a6ac29ccf326ec2849f1c8497
Paolo Mascetti <paolo.mascetti@radicalbit.io>
2017-12-18 17:45:31 +0100

    remove useless comments

--- nsdb-common/src/main/scala/io/radicalbit/nsdb/common/protocol/EndpointActorProtocol.scala
+++ nsdb-common/src/main/scala/io/radicalbit/nsdb/common/protocol/EndpointActorProtocol.scala
@@ -14,8 +14,6 @@ sealed trait SQLStatementResult extends EndpointOutputProtocol {
   def metric: String
 }
 
-// TODO: the result must be well structured
-// TODO: it must contain the schema (for both select and insert) and the final retrieved data (only for the select)
 case class SQLStatementExecuted(db: String, namespace: String, metric: String, res: Seq[Bit] = Seq.empty)
     extends SQLStatementResult
 case class SQLStatementFailed(db: String, namespace: String, metric: String, reason: String) extends SQLStatementResult
