c115cf1c67ec11ec308443f49c4d5fde00959c21
Saverio Veltri <saverio.veltri@radicalbit.io>
2018-01-19 12:26:14 +0100

    minor fix

--- start-cluster.sh
+++ start-cluster.sh
@@ -4,7 +4,7 @@ if [ -z "$1" ]; then echo "starting local cluster";
 else
 export AKKA_HOSTNAME=$1
 echo "starting remote cluster at address '$1'"; fi
-java -cp nsdb-cluster/target/scala-2.12/nsdb-cluster.jar:lib/* \
+java -cp nsdb-cluster/target/scala-2.12/nsdb-cluster.jar:$ABSOLUTE_PATH/lib/* \
     -Dlogback.configurationFile=conf/logback.xml \
     -DconfDir="$ABSOLUTE_PATH/conf" \
     io.radicalbit.nsdb.cluster.Cluster
