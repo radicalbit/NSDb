386ce233d0aed2995163bd7eb2dd02820db82705
Saverio Veltri <saverio.veltri@radicalbit.io>
2018-03-27 15:12:09 +0200

    add a little more documentation to security module

--- nsdb-security/src/main/scala/io/radicalbit/nsdb/security/Security.scala
+++ nsdb-security/src/main/scala/io/radicalbit/nsdb/security/Security.scala
@@ -7,15 +7,28 @@ import io.radicalbit.nsdb.security.http.{EmptyAuthorization, NSDBAuthProvider}
 
 import scala.util.{Failure, Success, Try}
 
+/**
+  * Trait to mix-in in order to have access to the configured authorization provider.
+  */
 trait NsdbSecurity {
 
   protected def logger: Logger
 
   def config: Config
 
+  /**
+    * configuration key to set if security is enabled or not.
+    */
   lazy val security              = config.getBoolean("nsdb.security.enabled")
+  /**
+    * configuration key to set the authorization provider FQCN.
+    */
   lazy val authProviderClassName = config.getString("nsdb.security.auth-provider-class")
 
+  /**
+    * Authorization provider instance, retrieved based on the configuration provided.
+    * If there is any error during the dynamic instantiation process, a [[Failure]] will be returned.
+    */
   lazy val authProvider: Try[NSDBAuthProvider] =
     if (!security) {
       logger.info("Security is not enabled")
--- nsdb-security/src/main/scala/io/radicalbit/nsdb/security/http/SecurityDirective.scala
+++ nsdb-security/src/main/scala/io/radicalbit/nsdb/security/http/SecurityDirective.scala
@@ -6,9 +6,9 @@ import akka.http.scaladsl.server.Route
 import io.radicalbit.nsdb.security.model.{Db, Metric, Namespace}
 
 /**
-  * Java friendly class for handling authorization process
-  * @param success: if authorization succeeded or not
-  * @param failReason: the fail reason
+  * Java friendly class for handling authorization process.
+  * @param success: if authorization succeeded or not.
+  * @param failReason: the fail reason.
   */
 case class AuthResponse(success: Boolean, failReason: String = "")
 
@@ -18,50 +18,74 @@ case class AuthResponse(success: Boolean, failReason: String = "")
 abstract class NSDBAuthProvider {
 
   /**
-    * name of the header to be retrieved from HTTP request
-    * @return the header name
+    * Header to be retrieved from HTTP request.
+    * @return header name.
     */
   def headerName: String
 
   /**
-    * method to check if a request against a Db is authorized
-    * @param ent the entity to check
-    * @param header the header to check; empty string if not present
-    * @param writePermission true if write permission is required
-    * @return authorization response
+    * Check if a request against a Db is authorized.
+    * @param ent the entity to check.
+    * @param header the header to check; empty string if not present.
+    * @param writePermission true if write permission is required.
+    * @return authorization response.
     */
   def checkDbAuth(ent: Db, header: String, writePermission: Boolean): AuthResponse
 
   /**
-    *method to check if a request against a Namespace is authorized
-    * @param ent the entity to check
-    * @param header the header to check; empty string if not present
-    * @param writePermission true if write permission is required
-    * @return authorization response
+    * Check if a request against a Namespace is authorized.
+    * @param ent the entity to check.
+    * @param header the header to check; empty string if not present.
+    * @param writePermission true if write permission is required.
+    * @return authorization response.
     */
   def checkNamespaceAuth(ent: Namespace, header: String, writePermission: Boolean): AuthResponse
 
   /**
-    * method to check if a request against a Metric is authorized
-    * @param ent the entity to check
-    * @param header the header to check; empty string if not present
-    * @param writePermission true if write permission is required
-    * @return authorization response
+    * Check if a request against a Metric is authorized.
+    * @param ent the entity to check.
+    * @param header the header to check; empty string if not present.
+    * @param writePermission true if write permission is required.
+    * @return authorization response.
     */
   def checkMetricAuth(ent: Metric, header: String, writePermission: Boolean): AuthResponse
 
+  /**
+    * Forward, if authorized, a request against a Db.
+    * @param ent the entity to check.
+    * @param header the header to check; empty string if not present.
+    * @param writePermission true if write permission is required.
+    * @param route the route to forward the request to.
+    * @return the destination route or a 403 if authorization check fails.
+    */
   final def authorizeDb(ent: Db, header: Option[String], writePermission: Boolean)(route: Route): Route = {
     val check = checkDbAuth(ent, header getOrElse "", writePermission)
     if (check.success) route
     else complete(HttpResponse(StatusCodes.Forbidden, entity = s"not authorized ${check.failReason}"))
   }
 
+  /**
+    * Forward, if authorized, a request against a Namespace.
+    * @param ent the entity to check.
+    * @param header the header to check; empty string if not present.
+    * @param writePermission true if write permission is required.
+    * @param route the route to forward the request to.
+    * @return the destination route or a 403 if authorization check fails.
+    */
   final def authorizeNamespace(ent: Namespace, header: Option[String], writePermission: Boolean)(route: Route): Route = {
     val check = checkNamespaceAuth(ent, header getOrElse "", writePermission)
     if (check.success) route
     else complete(HttpResponse(StatusCodes.Forbidden, entity = s"not authorized ${check.failReason}"))
   }
 
+  /**
+    * Forward, if authorized, a request against a Metric.
+    * @param ent the entity to check.
+    * @param header the header to check; empty string if not present.
+    * @param writePermission true if write permission is required.
+    * @param route the route to forward the request to.
+    * @return the destination route or a 403 if authorization check fails.
+    */
   final def authorizeMetric(ent: Metric, header: Option[String], writePermission: Boolean)(route: Route): Route = {
     val check = checkMetricAuth(ent, header getOrElse "", writePermission)
     if (check.success) route
@@ -70,6 +94,9 @@ abstract class NSDBAuthProvider {
 
 }
 
+/**
+  * Always allaw authorization provider. No checks are performed at all.
+  */
 class EmptyAuthorization extends NSDBAuthProvider {
 
   def headerName: String = "notRelevant"
