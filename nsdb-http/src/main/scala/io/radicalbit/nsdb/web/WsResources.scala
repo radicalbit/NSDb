b4592873d53d616a8d92d8049284ebf957543965
Paolo Mascetti <paolo.mascetti@radicalbit.io>
2018-04-09 14:45:04 +0200

    handling websocket messages

--- nsdb-http/src/main/scala/io/radicalbit/nsdb/web/WsResources.scala
+++ nsdb-http/src/main/scala/io/radicalbit/nsdb/web/WsResources.scala
@@ -8,13 +8,14 @@ import akka.http.scaladsl.model.ws.{Message, TextMessage}
 import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server._
-import akka.stream.OverflowStrategy
+import akka.stream._
 import akka.stream.contrib.TimeWindow
-import akka.stream.scaladsl.{Flow, Sink, Source}
+import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, Merge, Sink, Source}
+import io.radicalbit.nsdb.actors.PublisherActor.Events.RecordsPublished
 import io.radicalbit.nsdb.security.http.NSDBAuthProvider
 import io.radicalbit.nsdb.web.actor.StreamActor
 import io.radicalbit.nsdb.web.actor.StreamActor._
-import org.json4s._
+import org.json4s.DefaultFormats
 import org.json4s.jackson.JsonMethods.parse
 import org.json4s.jackson.Serialization.write
 
@@ -53,23 +54,62 @@ trait WsResources {
         }
         .to(Sink.actorRef(connectedWsActor, Terminate))
 
-    val outgoingMessages: Source[Message, NotUsed] =
-      Source
-        .actorRef[StreamActor.OutgoingMessage](retentionSize, OverflowStrategy.dropNew)
-        .mapMaterializedValue { outgoingActor =>
-          connectedWsActor ! StreamActor.Connect(outgoingActor)
-          NotUsed
-        }
-        .map {
-          case OutgoingMessage(message) =>
-            TextMessage(write(message))
-        }
+    val customSource: Graph[SourceShape[TextMessage.Strict], NotUsed] = GraphDSL.create() {
+      implicit builder: GraphDSL.Builder[NotUsed] =>
+        import GraphDSL.Implicits._
+
+        val outgoingMessages =
+          builder
+            .add(
+              Source
+                .actorRef[StreamActor.OutgoingMessage](10, OverflowStrategy.dropTail)
+                .mapMaterializedValue { outgoingActor =>
+                  connectedWsActor ! StreamActor.Connect(outgoingActor)
+                  NotUsed
+                }
+                .map {
+                  case msg @ OutgoingMessage(_) =>
+                    msg
+                })
+
+        val bcast: UniformFanOutShape[OutgoingMessage, OutgoingMessage] = builder.add(Broadcast[OutgoingMessage](2))
+
+        val merge: UniformFanInShape[OutgoingMessage, OutgoingMessage] = builder.add(Merge[OutgoingMessage](2))
+
+        val filterPublishMessage: FlowShape[OutgoingMessage, OutgoingMessage] =
+          builder.add(Flow[OutgoingMessage].filter { outgoing =>
+            outgoing.message.isInstanceOf[RecordsPublished]
+          })
+
+        val windowFlow: FlowShape[OutgoingMessage, OutgoingMessage] =
+          builder.add(TimeWindow(FiniteDuration(publishInterval, TimeUnit.MILLISECONDS), eager = true)(
+            identity[OutgoingMessage]) { (_, newMessage) =>
+            newMessage
+          })
+
+        val filterOthersMessage: FlowShape[OutgoingMessage, OutgoingMessage] =
+          builder
+            .add(
+              Flow[OutgoingMessage]
+                .filter { outgoing =>
+                  !outgoing.message.isInstanceOf[RecordsPublished]
+                })
+
+        val writeMessage: FlowShape[OutgoingMessage, TextMessage.Strict] =
+          builder.add(
+            Flow[OutgoingMessage]
+              .map { outgoing =>
+                TextMessage(write(outgoing.message))
+              })
+
+        outgoingMessages ~> bcast ~> filterPublishMessage ~> windowFlow ~> merge ~> writeMessage
+        bcast ~> filterOthersMessage ~> merge
+
+        SourceShape(writeMessage.out)
+    }
 
     Flow
-      .fromSinkAndSource(incomingMessages, outgoingMessages)
-      .via(
-        TimeWindow(FiniteDuration(publishInterval, TimeUnit.MILLISECONDS), eager = true)(identity[Message])(
-          (_, newMessage) => newMessage))
+      .fromSinkAndSource(incomingMessages, Source.fromGraph(customSource))
 
   }
 
