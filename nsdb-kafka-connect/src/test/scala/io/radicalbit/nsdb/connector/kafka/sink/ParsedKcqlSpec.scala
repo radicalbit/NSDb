d0c64944e70297549cca16071353b25dce44c68c
Saverio Veltri <saverio.veltri@radicalbit.io>
2018-06-02 11:28:10 +0200

    add support to `nsdb.defaultValue` config param

--- nsdb-kafka-connect/src/test/scala/io/radicalbit/nsdb/connector/kafka/sink/ParsedKcqlSpec.scala
+++ nsdb-kafka-connect/src/test/scala/io/radicalbit/nsdb/connector/kafka/sink/ParsedKcqlSpec.scala
@@ -25,59 +25,66 @@ class ParsedKcqlSpec extends FlatSpec with Matchers with OneInstancePerTest {
     an[IllegalArgumentException] should be thrownBy ParsedKcql(
       "INSERT INTO metric SELECT x AS a, y AS b, z AS c FROM topic",
       None,
+      None,
       None)
     an[IllegalArgumentException] should be thrownBy ParsedKcql(
       "INSERT INTO metric SELECT x AS a, y AS b, z AS c FROM topic WITHTIMESTAMP y",
+      None,
       None,
       None)
     an[IllegalArgumentException] should be thrownBy ParsedKcql(
       "INSERT INTO metric SELECT x AS db, y AS b, z AS c FROM topic WITHTIMESTAMP y",
       None,
+      None,
       None)
     an[IllegalArgumentException] should be thrownBy ParsedKcql(
       "INSERT INTO metric SELECT x AS db, y AS namespace, z AS c FROM topic WITHTIMESTAMP y",
       None,
+      None,
       None)
 
-    ParsedKcql("INSERT INTO metric SELECT x AS db, y AS namespace, z AS value FROM topic WITHTIMESTAMP y", None, None) shouldBe ParsedKcql(
-      "x",
-      "y",
-      "metric",
-      Map("value" -> "z", "timestamp" -> "y"))
+    ParsedKcql("INSERT INTO metric SELECT x AS db, y AS namespace, z AS value FROM topic WITHTIMESTAMP y",
+               None,
+               None,
+               None) shouldBe ParsedKcql("x", "y", "metric", None, Map("value" -> "z", "timestamp" -> "y"))
   }
 
   "KcqlFields" should "accept kcqls without db and namespace mapping but with global configs instead" in {
-    ParsedKcql("INSERT INTO metric SELECT z AS value FROM topic WITHTIMESTAMP y", Some("x"), Some("y")) shouldBe ParsedKcql(
+    ParsedKcql("INSERT INTO metric SELECT z AS value FROM topic WITHTIMESTAMP y", Some("x"), Some("y"), None) shouldBe ParsedKcql(
       "x",
       "y",
       "metric",
+      None,
       Map("value" -> "z", "timestamp" -> "y"))
   }
 
   "KcqlFields" should "successfully convert queries with or without aliases" in {
     val noDimensionQuery = "INSERT INTO metric SELECT x AS db, y AS namespace, z AS value FROM topic WITHTIMESTAMP y"
 
-    ParsedKcql(noDimensionQuery, None, None) shouldBe ParsedKcql("x",
-                                                                 "y",
-                                                                 "metric",
-                                                                 Map("value" -> "z", "timestamp" -> "y"))
+    ParsedKcql(noDimensionQuery, None, None, None) shouldBe ParsedKcql("x",
+                                                                       "y",
+                                                                       "metric",
+                                                                       None,
+                                                                       Map("value" -> "z", "timestamp" -> "y"))
 
     val withDimensionNoAlias =
       "INSERT INTO metric SELECT x AS db, y AS namespace, z AS value, d FROM topic WITHTIMESTAMP t"
 
-    ParsedKcql(withDimensionNoAlias, None, None) shouldBe ParsedKcql(
+    ParsedKcql(withDimensionNoAlias, None, None, None) shouldBe ParsedKcql(
       "x",
       "y",
       "metric",
+      None,
       Map("value" -> "z", "timestamp" -> "t", "d" -> "d"))
 
     val withDimensionAlias =
       "INSERT INTO metric SELECT x AS db, y AS namespace, z AS value, d as dimension FROM topic WITHTIMESTAMP y"
 
-    ParsedKcql(withDimensionAlias, None, None) shouldBe ParsedKcql(
+    ParsedKcql(withDimensionAlias, None, None, None) shouldBe ParsedKcql(
       "x",
       "y",
       "metric",
+      None,
       Map("value" -> "z", "timestamp" -> "y", "dimension" -> "d"))
   }
 
