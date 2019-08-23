05d68420d5728d464f54c7edb64822713d7dda87
Saverio Veltri <saverio.veltri@radicalbit.io>
2019-08-23 15:15:19 +0200

    separate children creation in metric data actor

--- nsdb-cluster/src/main/scala/io/radicalbit/nsdb/cluster/actor/MetricsDataActor.scala
+++ nsdb-cluster/src/main/scala/io/radicalbit/nsdb/cluster/actor/MetricsDataActor.scala
@@ -35,7 +35,7 @@ import io.radicalbit.nsdb.protocol.MessageProtocol.Events._
 import io.radicalbit.nsdb.util.ActorPathLogging
 import org.apache.commons.io.FileUtils
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 /**
   * Actor responsible for dispatching read or write commands to the proper actor and index.
@@ -48,54 +48,60 @@ class MetricsDataActor(val basePath: String, val nodeName: String, commitLogCoor
   lazy val readParallelism = ReadParallelism(context.system.settings.config.getConfig("nsdb.read.parallelism"))
 
   /**
-    * Gets or creates:
-    * - reader child actor of class [[MetricReaderActor]] to handle read requests
-    * - accumulator child actor of class [[MetricAccumulatorActor]] to handle write requests
+    * Gets or creates reader child actor of class [[MetricReaderActor]] to handle read requests
     *
     * @param db database name
     * @param namespace namespace name
-    * @return ([[MetricReaderActor]], [[MetricAccumulatorActor]]) for selected database and namespace
+    * @return [[MetricReaderActor]] for selected database and namespace
     */
-  private def getOrCreateChildren(db: String, namespace: String): (ActorRef, ActorRef) = {
-    val readerOpt      = context.child(s"metric_reader_${db}_$namespace")
-    val accumulatorOpt = context.child(s"metric_accumulator_${db}_$namespace")
+  private def getOrCreateReader(db: String, namespace: String): ActorRef =
+    context
+      .child(s"metric_reader_${db}_$namespace")
+      .getOrElse(
+        context.actorOf(
+          readParallelism.pool.props(
+            MetricReaderActor
+              .props(basePath, nodeName, db, namespace)
+              .withDispatcher("akka.actor.control-aware-dispatcher")),
+          s"metric_reader_${db}_$namespace"
+        ))
 
-    val reader = readerOpt.getOrElse(
-      context.actorOf(
-        readParallelism.pool.props(
-          MetricReaderActor
+  /**
+    * Gets or creates accumulator child actor of class [[MetricAccumulatorActor]] to handle write requests
+    *
+    * @param db database name
+    * @param namespace namespace name
+    * @return [[MetricAccumulatorActor]] for selected database and namespace
+    */
+  private def getOrCreateAccumulator(db: String, namespace: String): ActorRef =
+    context.child(s"metric_accumulator_${db}_$namespace").getOrElse {
+      val reader = context
+        .child(s"metric_reader_${db}_$namespace")
+        .getOrElse(context.actorOf(
+          readParallelism.pool.props(MetricReaderActor
             .props(basePath, nodeName, db, namespace)
             .withDispatcher("akka.actor.control-aware-dispatcher")),
-        s"metric_reader_${db}_$namespace"
-      ))
-    val accumulator = accumulatorOpt.getOrElse(
+          s"metric_reader_${db}_$namespace"
+        ))
       context.actorOf(MetricAccumulatorActor.props(basePath, db, namespace, reader, commitLogCoordinator),
-                      s"metric_accumulator_${db}_$namespace"))
-    (reader, accumulator)
+                      s"metric_accumulator_${db}_$namespace")
+    }
+
+  /**
+    * Gracefully stops reader and accumulator children.
+    * @param db database name
+    * @param namespace namespace name
+    */
+  private def stopChildren(db: String, namespace: String)(implicit ec: ExecutionContext) = {
+    Future.sequence(
+      Seq(
+        context.child(s"metric_reader_${db}_$namespace").map(gracefulStop(_, timeout.duration)).getOrElse(Future(true)),
+        context
+          .child(s"metric_accumulator_${db}_$namespace")
+          .map(gracefulStop(_, timeout.duration))
+          .getOrElse(Future(true))
+      ))
   }
-
-  /**
-    * Gets it exists:
-    * - reader child actor of class [[MetricReaderActor]] to handle read requests
-    * - accumulator child actor of class [[MetricAccumulatorActor]] to handle write requests
-    *
-    * @param db database name
-    * @param namespace namespace name
-    * @return (Option[[MetricReaderActor]], Option[[MetricAccumulatorActor]]) for selected database and namespace
-    */
-  private def getChildren(db: String, namespace: String): (Option[ActorRef], Option[ActorRef]) =
-    (context.child(s"metric_reader_${db}_$namespace"), context.child(s"metric_accumulator_${db}_$namespace"))
-
-  /**
-    * If exists, gets the reader for selected namespace and database.
-    * Use in case of read
-    *
-    * @param db database name
-    * @param namespace namespace name
-    * @return Option containing child actor of class [[MetricAccumulatorActor]]
-    */
-  private def getReader(db: String, namespace: String): Option[ActorRef] =
-    context.child(s"metric_reader_${db}_$namespace")
 
   implicit val timeout: Timeout = Timeout(
     context.system.settings.config.getDuration("nsdb.namespace-data.timeout", TimeUnit.SECONDS),
@@ -105,21 +111,14 @@ class MetricsDataActor(val basePath: String, val nodeName: String, commitLogCoor
 
   override def receive: Receive = {
     case DeleteNamespace(db, namespace) =>
-      val children = getChildren(db, namespace)
-      val f = children._2
-        .map(indexToRemove => indexToRemove ? DeleteAllMetrics(db, namespace))
-        .getOrElse(Future(AllMetricsDeleted(db, namespace)))
-      f.flatMap(_ => {
-          Future
-            .sequence(Seq(children._1.map(gracefulStop(_, timeout.duration)).getOrElse(Future(true)),
-                          children._2.map(gracefulStop(_, timeout.duration)).getOrElse(Future(true))))
-        })
+      (getOrCreateAccumulator(db, namespace) ? DeleteAllMetrics(db, namespace))
+        .flatMap(_ => stopChildren(db, namespace))
         .map(_ => NamespaceDeleted(db, namespace))
         .pipeTo(sender())
     case msg @ DropMetricWithLocations(db, namespace, _, _) =>
-      getOrCreateChildren(db, namespace)._2 forward msg
+      getOrCreateAccumulator(db, namespace) forward msg
     case msg @ EvictShard(db, namespace, _) =>
-      getOrCreateChildren(db, namespace)._2 forward msg
+      getOrCreateAccumulator(db, namespace) forward msg
     case CheckForOutDatedShards(db, namespace, locations) =>
       val locationGrouped = locations.groupBy(_.metric)
 
@@ -138,29 +137,27 @@ class MetricsDataActor(val basePath: String, val nodeName: String, commitLogCoor
           }
       }
 
-    case msg @ GetCountWithLocations(db, namespace, metric, _) =>
-      getReader(db, namespace) match {
-        case Some(child) => child forward msg
-        case None        => sender() ! CountGot(db, namespace, metric, 0)
-      }
+    case msg @ GetCountWithLocations(db, namespace, _, _) =>
+      getOrCreateReader(db, namespace) forward msg
     case msg @ ExecuteSelectStatement(statement, _, _, _) =>
-      getReader(statement.db, statement.namespace) match {
-        case Some(child) => child forward msg
-        case None        => sender() ! SelectStatementExecuted(statement.db, statement.namespace, statement.metric, Seq.empty)
-      }
-    case msg @ AddRecordToLocation(db, namespace, bit, location) =>
-      log.debug("received message {}", msg)
-      getOrCreateChildren(db, namespace)._2
-        .forward(AddRecordToShard(db, namespace, Location(location.metric, nodeName, location.from, location.to), bit))
+      getOrCreateReader(statement.db, statement.namespace) forward msg
+    case AddRecordToLocation(db, namespace, bit, location) =>
+      getOrCreateAccumulator(db, namespace) forward AddRecordToShard(
+        db,
+        namespace,
+        Location(location.metric, nodeName, location.from, location.to),
+        bit)
     case DeleteRecordFromLocation(db, namespace, bit, location) =>
-      getOrCreateChildren(db, namespace)._2
-        .forward(
-          DeleteRecordFromShard(db, namespace, Location(location.metric, nodeName, location.from, location.to), bit))
+      getOrCreateAccumulator(db, namespace) forward DeleteRecordFromShard(
+        db,
+        namespace,
+        Location(location.metric, nodeName, location.from, location.to),
+        bit)
     case ExecuteDeleteStatementInternalInLocations(statement, schema, locations) =>
-      getOrCreateChildren(statement.db, statement.namespace)._2.forward(
-        ExecuteDeleteStatementInShards(statement,
-                                       schema,
-                                       locations.map(l => Location(l.metric, nodeName, l.from, l.to))))
+      getOrCreateAccumulator(statement.db, statement.namespace) forward ExecuteDeleteStatementInShards(
+        statement,
+        schema,
+        locations.map(l => Location(l.metric, nodeName, l.from, l.to)))
   }
 
 }
