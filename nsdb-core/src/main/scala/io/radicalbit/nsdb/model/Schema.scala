0b5d609083158dc47b81309dea63da280fb39058
Saverio Veltri <saverio.veltri@radicalbit.io>
2020-03-03 15:17:13 +0000

    make schema primary constructor private. Allow schema creation only from bit

--- nsdb-core/src/main/scala/io/radicalbit/nsdb/model/Schema.scala
+++ nsdb-core/src/main/scala/io/radicalbit/nsdb/model/Schema.scala
@@ -17,7 +17,6 @@
 package io.radicalbit.nsdb.model
 
 import io.radicalbit.nsdb.common.NSDbType
-import io.radicalbit.nsdb.common.protocol.{Bit, FieldClassType}
 import io.radicalbit.nsdb.common.protocol.{Bit, FieldClassType, NSDbSerializable}
 import io.radicalbit.nsdb.index.{IndexType, TypeSupport}
 
@@ -53,7 +52,7 @@ case class SchemaField(name: String, fieldClassType: FieldClassType, indexType: 
   * @param metric the metric.
   * @param fieldsMap a map of [[SchemaField]] keyed by field name
   */
-case class Schema(metric: String, fieldsMap: Map[String, SchemaField]) extends NSDbSerializable {
+case class Schema private (metric: String, fieldsMap: Map[String, SchemaField]) extends NSDbSerializable {
   override def equals(obj: scala.Any): Boolean = {
     if (obj != null && obj.isInstanceOf[Schema]) {
       val otherSchema = obj.asInstanceOf[Schema]
