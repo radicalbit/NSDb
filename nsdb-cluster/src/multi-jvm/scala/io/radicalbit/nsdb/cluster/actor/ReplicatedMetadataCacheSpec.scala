2d23befebf66cecd21b9bece4f3c64f5d22c90f9
RiccardoDiomedi <riccardo.diomedi@radicalbit.io>
2020-03-04 14:47:08 +0100

    insert in conf write consistency: all or majority (#123)
    * insert in conf write consistency: all or majority
    
    * write consistency as external component
    
    * fix broken tests

--- nsdb-cluster/src/multi-jvm/scala/io/radicalbit/nsdb/cluster/actor/ReplicatedMetadataCacheSpec.scala
+++ nsdb-cluster/src/multi-jvm/scala/io/radicalbit/nsdb/cluster/actor/ReplicatedMetadataCacheSpec.scala
@@ -21,7 +21,7 @@ object ReplicatedMetadataCacheSpec extends MultiNodeConfig {
 
   commonConfig(ConfigFactory.parseString("""
     |akka.loglevel = ERROR
-    |akka.actor{
+    |akka.actor {
     | provider = "cluster"
     |
     | serialization-bindings {
@@ -33,14 +33,17 @@ object ReplicatedMetadataCacheSpec extends MultiNodeConfig {
     |   }
     |}
     |akka.log-dead-letters-during-shutdown = off
-    |nsdb{
+    |nsdb {
     |
+    |  global.timeout = 30 seconds
     |  read-coordinator.timeout = 10 seconds
     |  namespace-schema.timeout = 10 seconds
     |  namespace-data.timeout = 10 seconds
     |  publisher.timeout = 10 seconds
     |  publisher.scheduler.interval = 5 seconds
     |  write.scheduler.interval = 15 seconds
+    |
+    |  cluster.metadata-write-consistency = "all"
     |
     |  write-coordinator.timeout = 5 seconds
     |  metadata-coordinator.timeout = 5 seconds
