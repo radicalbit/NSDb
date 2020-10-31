c656d4997e3dc00c651cad60848ae73ba6f9034b
Saverio Veltri <saverio.veltri@radicalbit.io>
2020-10-31 10:26:06 +0100

    enable support for empty strings in parser

--- nsdb-sql/src/main/scala/io/radicalbit/nsdb/sql/parser/SQLStatementParser.scala
+++ nsdb-sql/src/main/scala/io/radicalbit/nsdb/sql/parser/SQLStatementParser.scala
@@ -133,8 +133,9 @@ final class SQLStatementParser extends RegexParsers with PackratParsers with Reg
   }, _ => "Distinct clause is only applicable to the count aggregation")
 
   private val dimension = standardString
-  private val stringValue = (specialString | (("'" ?) ~> (specialString +) <~ ("'" ?))) ^^ {
+  private val stringValue = (specialString | (("'" ?) ~> (specialString *) <~ ("'" ?))) ^^ {
     case string: String        => string
+    case Nil                   => ""
     case strings: List[String] => strings.mkString(" ")
   }
 
