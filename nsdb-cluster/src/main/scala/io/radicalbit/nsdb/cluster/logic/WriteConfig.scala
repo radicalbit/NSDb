791f9503b5ab881087a068652bc99591c837b012
Saverio Veltri <saverio.veltri@radicalbit.io>
2020-11-12 09:10:01 +0100

    Lightened location load at node startup (#178)
    * - add consistency in add location messages
    - add locations with local consistency at startup
    
    * fix unsubscribe for temporal queries
    
    * fix metadataconsistnecy serialization

--- nsdb-cluster/src/main/scala/io/radicalbit/nsdb/cluster/logic/WriteConfig.scala
+++ nsdb-cluster/src/main/scala/io/radicalbit/nsdb/cluster/logic/WriteConfig.scala
@@ -20,30 +20,37 @@ import java.util.concurrent.TimeUnit
 
 import akka.actor.Actor
 import akka.cluster.ddata.Replicator.{WriteAll, WriteConsistency, WriteLocal, WriteMajority}
+import com.fasterxml.jackson.core.{JsonGenerator, JsonParser}
+import com.fasterxml.jackson.databind.annotation.{JsonDeserialize, JsonSerialize}
+import com.fasterxml.jackson.databind.{DeserializationContext, SerializerProvider}
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer
+import com.fasterxml.jackson.databind.ser.std.StdSerializer
 import io.radicalbit.nsdb.common.configuration.NSDbConfig.HighLevel.globalTimeout
+import io.radicalbit.nsdb.common.protocol.NSDbSerializable
 
 import scala.concurrent.duration.FiniteDuration
 
 trait WriteConfig { this: Actor =>
+  import io.radicalbit.nsdb.cluster.logic.WriteConfig._
+  import MetadataConsistency._
+
   private val config = context.system.settings.config
 
   private lazy val timeout: FiniteDuration =
     FiniteDuration(config.getDuration(globalTimeout, TimeUnit.SECONDS), TimeUnit.SECONDS)
 
+  implicit def metadataConsistency2WriteConsistency(inputString: MetadataConsistency): WriteConsistency =
+    inputString match {
+      case All      => WriteAll(timeout)
+      case Majority => WriteMajority(timeout)
+      case Local    => WriteLocal
+    }
+
   /**
     * Provides a Write Consistency policy from a config value.
     */
-  protected lazy val metadataWriteConsistency: WriteConsistency = {
-    val configValue = config.getString("nsdb.cluster.metadata-write-consistency")
-
-    configValue.toLowerCase match {
-      case "all"      => WriteAll(timeout)
-      case "majority" => WriteMajority(timeout)
-      case "local"    => WriteLocal
-      case wrongConfigValue =>
-        throw new IllegalArgumentException(s"$wrongConfigValue is not a valid value for metadata-write-consistency")
-    }
-  }
+  protected lazy val metadataWriteConsistency: WriteConsistency =
+    MetadataConsistency(config.getString("nsdb.cluster.metadata-write-consistency"))
 
   /**
     * Parallel write-processing guarantees higher throughput while serial write-processing preserves the order of the operations.
@@ -52,14 +59,69 @@ trait WriteConfig { this: Actor =>
   case object Parallel extends WriteProcessing
   case object Serial   extends WriteProcessing
 
-  protected lazy val writeProcessing: WriteProcessing = {
-    val configValue = config.getString("nsdb.cluster.write-processing")
-
-    configValue.toLowerCase match {
+  implicit def string2WriteProcessing(inputString: String): WriteProcessing =
+    inputString.toLowerCase match {
       case "parallel" => Parallel
       case "serial"   => Serial
       case wrongConfigValue =>
         throw new IllegalArgumentException(s"$wrongConfigValue is not a valid value for write-processing")
+    }
+
+  protected lazy val writeProcessing: WriteProcessing = config.getString("nsdb.cluster.write-processing")
+
+}
+
+object WriteConfig {
+
+  object MetadataConsistency {
+
+    /**
+      * Metadata consistency
+      * - All: a metadata record is synchronously disseminated to all cluster nodes.
+      * - All: a metadata record is synchronously disseminated to the nodes, asynchronously to the remaining.
+      * - Local: a metadata record is written only to the local node and then asynchronously disseminate to the others.
+      */
+    @JsonSerialize(using = classOf[MetadataConsistencyJsonSerializer])
+    @JsonDeserialize(using = classOf[MetadataConsistencyJsonDeserializer])
+    sealed trait MetadataConsistency
+
+    case object All      extends MetadataConsistency with NSDbSerializable
+    case object Majority extends MetadataConsistency with NSDbSerializable
+    case object Local    extends MetadataConsistency with NSDbSerializable
+
+    def apply(inputString: String): MetadataConsistency =
+      inputString.toLowerCase match {
+        case "all"      => All
+        case "majority" => Majority
+        case "local"    => Local
+        case wrongConfigValue =>
+          throw new IllegalArgumentException(s"$wrongConfigValue is not a valid value for metadata-write-consistency")
+      }
+
+    /**
+      * Serializer for [[MetadataConsistency]]
+      */
+    class MetadataConsistencyJsonSerializer extends StdSerializer[MetadataConsistency](classOf[MetadataConsistency]) {
+
+      override def serialize(value: MetadataConsistency, gen: JsonGenerator, provider: SerializerProvider): Unit =
+        gen.writeString(value.toString)
+    }
+
+    /**
+      * Deserializer for [[MetadataConsistency]]
+      */
+    class MetadataConsistencyJsonDeserializer
+        extends StdDeserializer[MetadataConsistency](classOf[MetadataConsistency]) {
+
+      override def deserialize(p: JsonParser, ctxt: DeserializationContext): MetadataConsistency = {
+        p.getText match {
+          case "All"      => All
+          case "Majority" => Majority
+          case "Local"    => Local
+          case wrongValue =>
+            throw new IllegalArgumentException(s"$wrongValue is not a valid value for MetadataConsistency")
+        }
+      }
     }
   }
 }
